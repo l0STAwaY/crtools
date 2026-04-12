b87c9979260a53e9d724caefa58842397a87b17c
loSTAwaY <100456874+loSTAwaY@users.noreply.github.com>
2026-04-12 18:01:52 -0400

    Initialization for crtools  using devtool
    Using devtools tool create the basic documentation, add gitignore to filter privtae dataset

---
+++ R/sum_data.R
@@ -0,0 +1,30 @@
+library(dplyr)    
+#' Title
+#'
+#' @param data 
+#' @param response 
+#' @param group 
+#'
+#' @returns
+#' @import dplyr
+#' @export
+#'
+#' @examples
+
+sum_data <-  function(data, response, group=None){
+  
+# Lambda Summaries
+# Mean (lambda)
+# Variance (lambda)
+# Mean To Variance Ratio(lambda)
+# Zero Count Proportion(lambda)
+# Median and Quartiles(lambda)
+  
+  
+}
+
+
+
+y <- data()
+
+
